b66a406d09c5e771b15ad74ab1659b930c2666cb
rest24 <restu.andriyana.suhendar@gmail.com>
2023-08-18 00:42:02 +0700

    nambah lagi file

---
+++ dasar/elifstatment.py
@@ -0,0 +1,11 @@
+# ELIF = else if statement 
+nama = input ("Nama Anda : ")
+if nama == "ucup" : # kondisi 1
+    print(f"Hai ganteng") # aksi true 1
+elif nama == "otong" : # kondisi 2
+    print("Hei si kece banget") # aksi true 2
+elif nama == "mario": # kondisi 3
+    print("Hai humoris") # aksi true 3
+else :
+    print("Au ah ngga kenal") # aksi False
+print("Ini adalah akhir program")
---
+++ dasar/ifdanelsestatment.py
@@ -0,0 +1,29 @@
+# If dan Else Statment
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda ?")
+'''
+  program sebelumnya
+  if aksi : kondisi
+  Program selanjutnya
+'''
+
+# 1. program if inline
+# if nama == "ucup" : print ("Kamu ganteng abiieez ...!!")
+# print(f"Terimakasih {nama}")
+
+# 2. program if indentation
+if nama == "ucup" :
+    print("kamu keren Abieezz")
+    print("kamu juga keren bingit")
+print(f"Terimakasih {nama}")
+
+# 3. Else Statement
+if nama == "otong" :
+    print("hai otong, kamu keren..!!")
+else :
+    print("Kamu bukan otong,, ngga keren!")
+print("Akhir dari program")
